beaa16f6b31135154054d3ba88fe8880fba86ef9
Karol P <kpawlx@gmail.com>
2023-07-11 10:28:55 +0200

    Layout and sample subpages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<header>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+		</ul>
+		<ul>
+			<li><a href="/about">About</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</nav>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>&copy; 2023 kpawlx</footer>
